9ff5395b5dc455d01815af84aa65ee151c4fff68
qunwei <lee880716@gmail.com>
2026-07-24 17:51:53 +0800

    feat: initialize global wiki idempotently

--- src/kimi_cli/wiki/__init__.py
+++ src/kimi_cli/wiki/__init__.py
@@ -1,1 +1,13 @@
 """Shared, user-level Wiki support for OpenKimo."""
+
+from kimi_cli.wiki.initialize import UnsupportedWikiSchema, WikiLayout, ensure_wiki, layout_for
+from kimi_cli.wiki.paths import WIKI_SCHEMA_VERSION, resolve_wiki_root
+
+__all__ = [
+    "UnsupportedWikiSchema",
+    "WIKI_SCHEMA_VERSION",
+    "WikiLayout",
+    "ensure_wiki",
+    "layout_for",
+    "resolve_wiki_root",
+]
